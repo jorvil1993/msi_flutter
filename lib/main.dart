7305e6f3c75f7e3dc664996008289929043fef0b
unknown <jorvil.1993@gmail.com>
2019-06-05 10:07:34 -0400

    commit on 2th computer

--- lib/main.dart
+++ lib/main.dart
@@ -13,7 +13,7 @@ void main() {
       body: Center(
         child: Image(
           image: NetworkImage(
-            'https://assets3.thrillist.com/v1/image/2797371/size/tmg-article_default_mobile.jpg',
+            'https://amp.businessinsider.com/images/5b33c37a1ae6621d008b499c-750-562.jpg',
           ),
         ),
       ),
