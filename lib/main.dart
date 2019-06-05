107d25d62b839a119961293e891fe3c4aca75988
unknown <jorvil.1993@gmail.com>
2019-06-05 09:45:08 -0400

    first

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+//This is our first Hello World to see how MaterialApp actually works
+import 'package:flutter/material.dart';
+
+//the main function is the strating point for all our Flutter apps.
+void main() {
+  runApp(MaterialApp(
+    home: Scaffold(
+      appBar: AppBar(
+        title: Text('I am Rich'),
+        backgroundColor: Colors.blueGrey[900],
+      ),
+      backgroundColor: Colors.blueGrey,
+      body: Center(
+        child: Image(
+          image: NetworkImage(
+            'http://hdqwalls.com/wallpapers/manhattan-new-york-city-4k-je.jpg',
+          ),
+        ),
+      ),
+    ),
+  ));
+}
